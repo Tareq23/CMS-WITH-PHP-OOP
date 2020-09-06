4079595893606d0af0992e0e9b1ca3e7170e29e0
TAREQ <mdtarequlislam634@gmail.com>
2020-09-06 18:07:07 +0600

    declare insert,delete,update,query function and these completed

--- classes/DB.php
+++ classes/DB.php
@@ -1,21 +1,19 @@
 <?php 
 
-
-
 class DB{
 
     private static $_connect = null;
-    private $_pdo;
+    private $_pdo,$_count=0,$_result=null,$_error=false,$_stmt;
 
     public function __construct()
     {
         try{
             $this->_pdo = new PDO("mysql:host=localhost;dbname=cms","root","");
-            echo "successfully connected";
+            //echo "successfully connected";
         }
         catch(PDOException $e)
         {
-            echo "something worng";
+            //echo "something worng";
             die($e->getMessage());
         }
     }
@@ -27,10 +25,158 @@ class DB{
         }
         return self::$_connect;
     }
+
+    private function query($sql,$fields = array())
+    {   
+        $this->_count=0;
+        $this->_error=false;
+        $this->_result = null;
+        if($this->_stmt = $this->_pdo->prepare($sql))
+        {
+            if(count($fields))
+            {
+                $index = 1;
+                foreach($fields as $field)
+                {
+                    $this->_stmt->bindParam($index,$field);
+                    $index++;
+                }
+            }
+            if($this->_stmt->execute())
+            {
+                $this->_count = $this->_stmt->rowCount();
+            }else{
+                $this->_error = true;
+            }
+        }
+        return $this;
+    }
+    public function error()
+    {
+        return $this->_error;
+    }
+    public function count()
+    {
+        return $this->_count;
+    }
+    public function result()
+    {
+        return $this->_result;
+    }
+    public function fetch()
+    {
+        $this->_result = $this->_stmt->fetch(PDO::FETCH_OBJ);
+        return $this;
+    }
+    public function fetchAll()
+    {
+        $this->_result = $this->_stmt->fetchAll(PDO::FETCH_OBJ);
+        return $this;
+    }
+    public function insert($table,$fields)
+    {
+        if(count($fields))
+        {
+            $columns = '';
+            $value = '';
+            $cnt = 1;
+            foreach($fields as $key => $field)
+            {
+                $columns .= $key;
+                $value .='?';
+                if($cnt<count($fields))
+                {
+                    $columns .= ',';
+                    $value .= ',';
+                }
+                $cnt++;
+            }
+            $sql = "INSERT INTO {$table} {$columns} {$value}";
+            return $this->query($sql,$fields);
+        }
+        else{
+            $this->_count = 0;
+        }
+    }
+    private function checkOperator($operator='')
+    {
+        $operators = array('>','<','>=','<=','=');
+        return (in_array($operator,$operators))?true:false;
+    }
+    public function delete($table=null,$operator,$fields=array())
+    {
+        if(count($fields)&&$this->checkOperator($operator))
+        {
+            $condition_field = array_keys($fields);
+            $sql = "DELETE FROM {$table} WHERE {$condition_field} {$operator} ?";
+            return $this->query($sql,$fields);
+        }
+    }
+    public function update($table=null,$fields=array(),$operator='',$condition = array())
+    {
+        if(count($fields)&&$this->checkOperator($operator))
+        {
+            $columns = '';
+            $cnt = 1;
+            $value = '';
+            $condition_field = array_keys($condition);
+            foreach($fields as $key => $field)
+            {
+                $columns .= $key . '=?';
+                if($cnt<count($fields))
+                {
+                    $columns .= ',';
+                    $value .= ',';
+                }
+            }
+            $sql = "UPDATE {$table} SET {$columns} WHERE {$condition_field} {$operator} {$condition[$condition_field]}";
+            return $this->query($sql,$fields);
+        }
+
+    }
+    public function get($table,$fields=array(),$operator,$value = array())
+    {
+        if(count($fields)&&$this->checkOperator($operator))
+        {
+            $columns = '';
+            $cnt = 1;
+            $condition_field = array_keys($value);
+            foreach($fields as $key => $field)
+            {
+                $columns .= $key;
+                if($cnt<count($fields))
+                {
+                    $columns .= ',';
+                }
+            }
+            $sql = "SELECT {$columns} FROM {$table} WHERE {$condition_field} {$operator} {$value[$condition_field]}";
+            return $this->query($sql,$fields);
+        }
+
+    }
+    public function getAll($table)
+    {
+        $sql = "SELECT * FROM {$table}";
+        return $this->query($sql);
+    }
+    public function addTable()
+    {
+
+    }
+    public function deleteTable()
+    {
+
+    }
+    public function addColumn()
+    {
+
+    }
+    public function deleteColumn()
+    {
+
+    }
+    public function updateColumn()
+    {
+
+    }
 }
-
-DB::connect();
-
-
-
-
